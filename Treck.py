2fffb8cf957b9231bb78b518ef619756de71735a
IvashovaVD <99237542+IvashovaVD@users.noreply.github.com>
2022-02-08 12:55:34 +0300

    Update and rename README.md to Treck.py

--- Treck.py
+++ Treck.py
@@ -1,11 +1,11 @@
 # IvashovaVD-mtuci-lab-1
-ROST=160
-VES=160
-KOLSH=60
-VAC=30
-v=ROST/4+0.37
-v=KOLSH/v
-rs=0.035*VES+(v**2/ROST)*0.029*VES
+ROST = 160
+VES = 160
+KOLSH = 60
+VAC = 30
+v = ROST/4+0.37
+v = KOLSH/v
+rs = 0.035*VES+(v**2/ROST)*0.029*VES
 print(f"Калорий сожено:{v}; Пройдено:{rs}")
 
 
