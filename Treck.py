ed055e6ce7e5ac1b188da30982cff3e2ab602cea
IvashovaVD <99237542+IvashovaVD@users.noreply.github.com>
2022-02-08 13:24:18 +0300

    feature: Постановка условия

--- Treck.py
+++ Treck.py
@@ -1,12 +1,27 @@
 # IvashovaVD-mtuci-lab-1
-ROST = 160
-VES = 160
-KOLSH = 60
-VAC = 30
-v = ROST/4+0.37
-v = KOLSH/v
-rs = 0.035*VES+(v**2/ROST)*0.029*VES
-print(f"Калорий сожено:{v}; Пройдено:{rs}")
+GROWTH = 160
+WEIGHT = 160
+STEPS = 60
+ACTIVITY = 30
+len = GROWTH/4+0.37
+len = STEPS/len
+distance = 0.035*WEIGHT+(len**2/GROWTH)*0.029*WEIGHT
+print(f"Калорий сожено:{len}; Пройдено:{distance}")
+if distance < 2:
+    print('Меньше 2х км')
+elif distance < 4:
+    print('Меньше 4х км')
+if distance > 4:
+    print('Больше 4х км')GROWTH = 160
+WEIGHT = 160
+STEPS = 60
+ACTIVITY =30
+len = GROWTH /4+0.37
+len = STEPS / len
+distance = 0.035*WEIGHT+(len**2/GROWTH)*0.029*WEIGHT
+print(f"Калорий сожено:{len}; Пройдено:{distance}")
+
 
 
 #Калорий сожено:1.4862521674510776; Пройдено:5.664059419652339
+#Больше 4х км
