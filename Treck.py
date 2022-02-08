5c34ad8bc3d9e481e9ecc70591f8675c034e8c8a
IvashovaVD <99237542+IvashovaVD@users.noreply.github.com>
2022-02-08 13:26:11 +0300

    Update Treck.py

--- Treck.py
+++ Treck.py
@@ -13,13 +13,6 @@ elif distance < 4:
     print('Меньше 4х км')
 if distance > 4:
     print('Больше 4х км')GROWTH = 160
-WEIGHT = 160
-STEPS = 60
-ACTIVITY =30
-len = GROWTH /4+0.37
-len = STEPS / len
-distance = 0.035*WEIGHT+(len**2/GROWTH)*0.029*WEIGHT
-print(f"Калорий сожено:{len}; Пройдено:{distance}")
 
 
 
