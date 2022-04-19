4bbd24e77ac1359653d982f95fb9a885e21243a2
IvashovaVD <99237542+IvashovaVD@users.noreply.github.com>
2022-04-19 14:19:02 +0300

    Create mtuci-seminar-js-2.js

---
+++ mtuci-seminar-js-2.js
@@ -0,0 +1,11 @@
+var arr = [3,2,5,6];
+function arraySum(array){ 
+  var sum = 0; 
+  for(var i = 0; i < array.length; i++){ 
+    sum += array[i];
+  } 
+  console.log(sum); 
+} 
+arraySum(arr);
+
+//16
